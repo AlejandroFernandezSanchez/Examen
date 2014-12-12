e889caa56b068d2fc507deab3007ca0e96b22013
AlejandroFernandezSanchez <alexsolo007@hotmail.com>
2014-12-12 13:27:14 +0100

    Commit3: Añadidos los porcentajes y el atributo gramos

--- Usuario.java
+++ Usuario.java
@@ -4,16 +4,18 @@
  */
 public class Usuario
 {    
-   
+    //nombre del usuario
     private String nombre;
-         
+    //gramos de proteinas ingeridos
     private float proteinas;
-    
+    //gramos de carbohidratos ingeridos
     private float carbohidratos;
-    
+    //gramos de grasas ingeridos
     private float grasas;
-    
+    //gramos de calorias ingeridos
     private float calorias;
+    //gramos de comida total ingerida
+    private float gramos;
 
     /**
      * Constructor de la clase
@@ -28,21 +30,37 @@ public class Usuario
      */
     public void estado()
     {
-        System.out.println("Nombre:                                         " + nombre );        
-        System.out.println("gramos de proteinas ingeridos:                  " + proteinas );
-        System.out.println("gramos de carbohidratos ingeridos:              " + carbohidratos );
-        System.out.println("gramos de grasas ingeridos:                     " + grasas );
+        String strProt="gramos de proteinas ingeridos:                  " + proteinas;
+        if (proteinas>0)
+        {
+            strProt=strProt  +" (" + (proteinas*100/gramos) + "%)";
+        }
+        String strCar="gramos de carbohidratos ingeridos:              " + carbohidratos;
+        if (carbohidratos>0)
+        {
+            strCar=strCar  +" (" + (carbohidratos*100/gramos) + "%)";
+        }
+        String strGra="gramos de grasas ingeridos:                     " + grasas;
+        if (grasas>0)
+        {
+            strGra=strGra  +" (" + (grasas*100/gramos) + "%)";
+        }
+        System.out.println(strProt);
+        System.out.println(strCar);
+        System.out.println(strGra);
         System.out.println("caloraías ingeirdas:                            " + calorias ); 
     }
     
     /**
      * Hace comer al usuario un tipo de comida y una cierta cantidad del mismo ( en gramos).
      */
-    public void comer(Alimento comida, int gr)
+    public void comer(Alimento comida, float gr)
     {
-        proteinas=proteinas+comida.getProteinas()*gr;
-        grasas=grasas+comida.getGrasas()*gr;
-        carbohidratos=carbohidratos+comida.getCarbohidratos()*gr;
-        calorias=calorias+comida.getCalorias()*gr;
+        float grCien = gr/100;
+        proteinas=proteinas+comida.getProteinas()*grCien;        
+        grasas=grasas+comida.getGrasas()*(gr/100);       
+        carbohidratos=carbohidratos+comida.getCarbohidratos()*grCien;             
+        calorias=calorias+comida.getCalorias()*grCien;
+        gramos=gr;
     }
 }
